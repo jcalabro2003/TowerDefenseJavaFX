041a41ae3ddf1bb8fa097f42976d01f2e2b92d57
LittleSamourai <samiirtiouririne@gmail.com>
2020-05-05 14:14:58 +0200

    Salutations

--- src/sample/model/PNJFactory.java
+++ src/sample/model/PNJFactory.java
@@ -10,15 +10,15 @@ public class PNJFactory {
         PNJ res = null;
         switch (type){
             case ("basic"):
-                imageView = LoadingImage.loadImage("Mario.png");
+                imageView = LoadingImage.loadImage("Mario.png",30,30);
                 res = new PNJ(30, 3, imageView);
                 break;
             case ("tank"):
-                imageView = LoadingImage.loadImage("Yoshi.png");
+                imageView = LoadingImage.loadImage("Yoshi.png",30,30);
                 res = new PNJ(100, 1, imageView);
                 break;
             case ("fast"):
-                imageView = LoadingImage.loadImage("Toad.png");
+                imageView = LoadingImage.loadImage("Toad.png",30,30);
                 res = new PNJ(10, 10, imageView);
                 break;
             default: break;
