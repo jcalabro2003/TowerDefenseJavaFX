60a84f4f964e6d8327cd6915f8e8ba8e1e672eee
LittleSamourai <samiirtiouririne@gmail.com>
2020-05-09 17:49:14 +0200

    oui

--- src/sample/model/Path.java
+++ src/sample/model/Path.java
@@ -28,32 +28,18 @@ public class Path {
             case ("map2") :
                 switch (typePath) {
                     case ("path1") :
-                        points.add(new Point(225, 75));
                         points.add(new Point(950, 75));
                         break;
                     case ("path2") :
                         points.add(new Point(225, 75));
-                        points.add(new Point(225, 175));
-                        points.add(new Point(475, 175));
-                        points.add(new Point(475, 50));
-                        points.add(new Point(950, 50));
+                        points.add(new Point(225, 275));
+                        points.add(new Point(325, 275));
+                        points.add(new Point(325, 175));
+                        points.add(new Point(625, 175));
+                        points.add(new Point(625, 75));
+                        points.add(new Point(950, 75));
                         break;
                 }
-
-                /*
-                points.add(new Point(175, 75));
-                points.add(new Point(175, 325));
-                points.add(new Point(775, 325));
-                points.add(new Point(775, 75));
-                points.add(new Point(950, 50));
-                points.add(new Point(325, 75));
-                points.add(new Point(325, 175));
-                points.add(new Point(325, 525));
-                points.add(new Point(725, 525));
-                points.add(new Point(725, 50));
-                 */
-                break;
-            default:
                 break;
         }
 
@@ -71,8 +57,8 @@ public class Path {
                 }
                 break;
             case ("map2") :
-                if ((y == 50 && x >= 0 && x <= 950) || (x==200 && y>=50 && y<=150) || (y == 150 && x >= 200 && x <= 450)
-                        || (x==450 && y>=50 && y<=150)  ) {
+                if ((y == 50 && x >= 0 && x <= 950) || (x==200 && y>=50 && y<=250) || (y == 250 && x >= 200 && x <= 300)
+                        || (x==300 && y>=150 && y<=250)|| (y==150 && x>=300 && x<=600) || (x==600 && y>=50 && y<=150) ) {
                     isPath = true;
                 }
                 break;
